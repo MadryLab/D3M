31edf9bca369c0a554aac1e6bf21abbfdc196612
Kristian Georgiev <krisgrg@mit.edu>
2024-01-31 16:33:02 -0500

    fix

--- setup.py
+++ setup.py
@@ -10,11 +10,6 @@ setup(
     author_email="krisgrg@mit.edu",
     license_files=("LICENSE.txt",),
     packages=["dda"],
-    install_requires=[
-        "torch>=2.0.0",
-        "numpy",
-        "tqdm",
-        "trak>=0.3.2"
-    ],
+    install_requires=["torch>=2.0.0", "numpy", "tqdm", "traker>=0.3.2"],
     include_package_data=True,
 )
